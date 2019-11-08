a725e41b982a0bb98c160b26a2d0aa12e9f8ccd9
prempador <muellner.benjamin@gmail.com>
2019-11-08 11:53:07 +0100

    Remove example and insert code and tests of generic client. Update readme

---
+++ kassensichv-client-ios/kassensichv_client_ios.h
@@ -0,0 +1,17 @@
+//
+//  kassensichv_client_ios.h
+//  kassensichv-client-ios
+//
+//  Created by Benjamin Müllner on 31.10.19.
+//  Copyright © 2019 fiskaly. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for kassensichv_client_ios.
+FOUNDATION_EXPORT double kassensichv_client_iosVersionNumber;
+
+//! Project version string for kassensichv_client_ios.
+FOUNDATION_EXPORT const unsigned char kassensichv_client_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <kassensichv_client_ios/PublicHeader.h>
